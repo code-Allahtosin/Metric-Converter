b52fadb10b5f779a631e55d5de6a29f2a9919b99
code-Allahtosin <107750118+code-Allahtosin@users.noreply.github.com>
2022-07-26 22:37:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+const convertBtn=   document.getElementById('convert-btn')
+const length=       document.getElementById('length')
+const volume=       document.getElementById('volume')
+const mass=         document.getElementById('mass')
+
+
+convertBtn.addEventListener('click', function(){
+  const input=Number(document.getElementById('input').value)
+  const metres=   (input/3.281).toFixed(3)
+  const feet=     (input*3.281).toFixed(3)
+  const liter=    (input/0.264).toFixed(3)
+  const gallon=   (input*0.264).toFixed(3)
+  const kilogram= (input/2.204).toFixed(3)
+  const pound=    (input*2.204).toFixed(3)
+    
+  if (input && input != 1){  
+
+    length.textContent= `${input} meters = ${feet} feet | ${input} feet = ${metres} meters`
+    volume.textContent= `${input} liters = ${gallon} gallons | ${input} gallons = ${liter} liters`
+    mass.textContent= `${input} kilos = ${pound} pounds | ${input} pounds = ${kilogram} kilos`
+    document.getElementById('input').value=''
+  } else {
+    length.textContent= `1 meter = 3.281 feet | 1 foot = 0.375 meter`
+    volume.textContent= `1 liter = 0.264 gallon | ${1} gallon = 3.788 liters`
+    mass.textContent= `1 kilo = 2.204 pounds | ${1} pound = 0.454 kilos`
+    document.getElementById('input').value=''
+  } 
+})
+    
+    
